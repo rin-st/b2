fc4ac1c77ee2bafc6b4d8ff8139318c4ce96e603
Rinat A <rinatkins@gmail.com>
2017-08-23 01:11:28 +0300

    first

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+const requestIp = require('request-ip');
+const os = require('os');
+
+app.get("/", function (request, response) {
+  var ip = requestIp.getClientIp(request);
+  var lg = request.headers["accept-language"].split(',')[0];
+  var sw = request.headers["user-agent"].split(/\(|\)/)[1];
+  var obj = {
+    "ipaddress" : ip,
+    "language" : lg,
+    "software" : sw
+  }
+   response.send(obj);
+});
+var listener = app.listen(process.env.PORT, function(){
+  console.log('Your app is listening on port ' + listener.address().port);
+});
